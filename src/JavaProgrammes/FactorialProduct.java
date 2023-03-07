a592ecbc805d1dc66bba4534c498f2d304fff24f
swar18320 <swapnil18320@gmail.com>
2023-03-07 19:24:07 +0530

    first commit

--- src/JavaProgrammes/FactorialProduct.java
+++ src/JavaProgrammes/FactorialProduct.java
@@ -1,5 +1,13 @@
 package JavaProgrammes;
 
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FactorialProduct {
 
 	public static void main(String[] args) {
@@ -14,7 +22,19 @@ public class FactorialProduct {
 			
 		}
 		System.out.println(fact);
-		
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver = new ChromeDriver();
+//		//driver.get("https://google.com");
+//		driver.navigate().to("https://skpatro.github.io/demo/links/");
+//			
+//		driver.findElement(By.xpath("//a[@href=\"JavaScript:newPopup('http://qavalidation.com/about-me/');\"]")).click();
+//		
+//		String mainid = driver.getWindowHandle();
+//		String title = driver.getTitle();
+//		System.out.println(title);
+//		
+//		Set<String> Child = driver.getWindowHandles();
+//		System.out.println(driver.getTitle());
 	}
 
 }
